a406be36677623e4a3caa0047d7a929c87955989
ltt4220 <1315216047@qq.com>
2017-05-22 14:19:44 +0800

    kljlkj

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//Javascript;
+$(function(){
+			$("#super").fullpage({
+				"anchors": ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9', 'page10'],
+				"navigation": true,
+				"navigationTooltips": ['首页', '视觉', '交互', '皮肤', '功能', '待办邮件', '联系人邮件', '科技', '连接易信', '马上体验']
+			});
+		});
